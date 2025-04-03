e70df9264a33a49ca34d4c2832418ff83abb1161
Mahmoud Ramadan <72145545+mramadan18@users.noreply.github.com>
2025-04-03 03:20:06 +0200

    implement Tailwind CSS setup and utility functions; update layout and global styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
+const roboto = Roboto({
   variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
   subsets: ["latin"],
 });
 
@@ -24,10 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        cz-shortcut-listen="true"
-      >
+      <body className={roboto.className} cz-shortcut-listen="true">
         {children}
       </body>
     </html>
